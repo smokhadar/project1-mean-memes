88108d1233e4f0a108869da862f86ffdafc546aa
Nathan <sabinnat001@gmail.com>
2023-04-24 21:14:14 -0700

    Pulled API

---
+++ script.js
@@ -0,0 +1,17 @@
+var APIKey = "OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5";
+var giphyUrl = "http://api.giphy.com/v1/gifs/search?api_key=OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5&q=" + userInput;
+console.log("hello");
+
+function getApi(){
+    fetch(giphyUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+    })
+
+} 
+
+getApi()
