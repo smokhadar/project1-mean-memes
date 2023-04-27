dca94b44b5cb727f0d0b418cde77b74099e3de2c
Nathan <sabinnat001@gmail.com>
2023-04-26 20:38:33 -0700

    Loaded both APIs

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 var APIKey = "OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5";
-var giphyUrl = "http://api.giphy.com/v1/gifs/search?api_key=OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5&q=" + userInput;
-console.log("hello");
+var giphyUrl = "http://api.giphy.com/v1/gifs/search?api_key=OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5&q=cake";
 
-function getApi(){
+var yeUrl = "https://api.kanye.rest";
+
+function giphyApi(){
     fetch(giphyUrl)
     .then(function(response){
         return response;
     })
     .then(function(data){
         console.log(data);
-
     })
 
 } 
 
-getApi()
+function yeApi(){
+    fetch(yeUrl)
+    .then(function(response){
+        // console.log(response); 
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
+
+
+//giphyApi()
+yeApi()
 
 // PULL request :)
