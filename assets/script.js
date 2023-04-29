30b6f74cb0614286c34e581a97aa1ca027ad04b6
Nathan <sabinnat001@gmail.com>
2023-04-29 11:14:08 -0700

    set up local storage for quotes

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,56 @@
-// pulls off of id tags in index.html.
-var imageBox = document.querySelector(".projectContent").children[0].children[0];
-var imageText = document.querySelector(".projectContent").children[1];
-var recipientName;
-var userMood; 
+// elements to render generated content in
+
+
+//need and image tag
+// var imageBox = document.querySelector("#projectContent").children[0].children[0];
+var imageText = document.querySelector("#projectContent").children[0];
+
+
+var recipientName = document.getElementById('recipientName');
+var userMood = document.getElementById('moods-giphy');
+
+
+var moodIndex = userMood.selectedIndex;
+var moodOptions = userMood.options;
+console.log(moodOptions[1].value);
+console.log(moodIndex);
+for (moodIndex; moodIndex < 4; moodIndex++) {
+    [moodIndex]
+}
+
+
+// var selectedMood = userMood.options[moodIndex];
+alert("Index: " + moodIndex + " is " + moodOptions[moodIndex].value);
+
+
+// console.log("moodIndex", moodIndex);
+// console.log("moodIndex", moodOptions.value);
+
+
+
+
+// event listener for saveBtn
+var saveBtn = document.querySelector('#projectContent').children[2];
+saveBtn.addEventListener('click', function(event) {
+    var userMood =
+    console.log(userMood);
+    giphyApi();
+    yeApi();
+});
 
 
 // API URLs
 var APIKey = "OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5";
-var giphyUrl = "http://api.giphy.com/v1/gifs/search?api_key=OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5&q=" + userInput;
-console.log("hello");
+var giphyUrl = "http://api.giphy.com/v1/gifs/search?api_key=OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5&q=cheeseburger";
 var giphyUrl = "http://api.giphy.com/v1/gifs/search?api_key=OfyI3KoCiM3YTdXVxfbOmwVxvhX0NUt5&q=cake";
 var yeUrl = "https://api.kanye.rest";
+
+
+
+
+var lastSavedGif;
+var lastSavedQuote;
+
 
 // function getApi(){
 // Pulls giphy api data.
@@ -21,30 +61,48 @@ function giphyApi(){
     })
     .then(function(data){
         console.log(data);
+       
 
+
+
+
+        lastSavedGif = JSON.parse(localStorage.getItem("lastSavedGif")) || [];
+        lastSavedGif.push({})
     })
 
-} 
 
-getApi()
-// Pulls kayne quote. 
+}
+
+
+// Pulls kayne quote.
+var textdatabase
+
+
 function yeApi(){
     fetch(yeUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
+        imageText.textContent = data.quote;
+        textdatabase = JSON.parse(localStorage.getItem("text")) || [];
+        textdatabase.push({text: data.quote});
+        localStorage.setItem("text", JSON.stringify(textdatabase));
+        //console.log(textdatabase);
     })
 }
 
+
+
+
 // add event listeners to submit button, favorites click, most recent click
 // grab the user input from form for name
-// grab user choice from dropdown
+
 
 // save generated gif and quote to local storage
 // render generated gif and quote to browser
 // render on "Most recent tab"
+
 
 // grab user choice of "favoriting" generated content
 // save to local storage
