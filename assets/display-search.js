a3484751c5335dd93545be71642a2db863e6a44c
Nathan <sabinnat001@gmail.com>
2023-05-01 19:54:47 -0700

    started favorites

--- assets/display-search.js
+++ assets/display-search.js
@@ -119,6 +119,23 @@ function handleAddToFavorites(quote, gif) {
     localStorage.setItem("favGifs", gif);
 };
 
+function loadFavorites(){
+    var favoriteQuotes = JSON.parse(localStorage.getItem("favQuotes"));
+    var favoriteGifs = JSON.parse(localStorage.getItem("favGifs"));
+
+    var favoritesTable = document.createElement("div");
+    favoriteTable.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+    for (var i = 0; i < favoriteQuotes.length; i++) {
+        var favoritesBody = document.createElement('div');
+        resultBody.classList.add('card-body');
+        favoritesBody.textContent = favoriteQuotes[i];
+        favoritesTable.append(resultBody);
+
+        
+    }
+}
+
 var addFavBtn = document.querySelector("#addFav")
 
 addFavBtn.addEventListener('click', handleAddToFavorites);
