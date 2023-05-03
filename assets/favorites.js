719e347460166b89712813ac4abbe23f33f9023c
Nathan <sabinnat001@gmail.com>
2023-05-02 21:12:31 -0700

    fixed function to save favorites, added unfinished function to insert all of them into the other page

--- assets/favorites.js
+++ assets/favorites.js
@@ -1,22 +1,41 @@
-// imports function 
-import { printResults } from '/display-search.js';
-
 // will load in all of the favorites and add them to page
 function loadFavorites(){
-    var favoriteQuotes = JSON.parse(localStorage.getItem("favQuotes"));
-    var favoriteGifs = JSON.parse(localStorage.getItem("favGifs"));
+    var favoriteQuotes = JSON.parse(localStorage.getItem("faveQuotes")) || "\0";
+    var favoriteGifs = JSON.parse(localStorage.getItem("faveGifs")) || "\0";
+
+    if (!favoriteQuotes || !favoriteGifs) {
+        return sadKanye;
+    }
 
     var favoritesTable = document.createElement("div");
-    favoriteTable.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
+    favoritesTable.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+    
     for (var i = 0; i < favoriteQuotes.length; i++) {
-        var favoritesBody = document.createElement('div');
-        resultBody.classList.add('card-body');
+        var favoritesBody = document.createElement("div");
+        favoritesBody.classList.add('card-body');
         favoritesBody.textContent = favoriteQuotes[i];
-        favoritesTable.append(resultBody);
+        favoritesTable.appendChild(favoritesBody);
 
-        printResults(favoriteGifs[i]);
+        console.log(favoriteQuotes[i]);
+        console.log(favoriteGifs[i]);
+
+        var favoritesGif = document.createElement("img");
+        favoritesGif.setAttribute('src', favoriteGifs[i].images);
+        favoritesGif.setAttribute('height', "100px");
+        favoritesGif.setAttribute('width', "100px");
+        favoritesTable.append(favoriteGifs);
     }
+    document.body.append(favoritesTable);
 }
+
+// Load favorites will call this function if the user does not have any favorites.
+function sadKanye() {
+    var noData = documet.createElement("div");
+    noData.textContent = "You have no favorites. :(";
+    document.body.append(noData);
+}
+
+// to reset local storage
+// localStorage.clear();
 
 loadFavorites()
